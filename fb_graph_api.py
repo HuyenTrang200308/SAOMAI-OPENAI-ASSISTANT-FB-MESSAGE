9c80b50ac9ea3eb8839e817197b82e065a28e483
Le Thi Huyen Trang <trang@example.com>
2025-06-30 11:40:04 +0700

    Noi dung sua doi

--- fb_graph_api.py
+++ fb_graph_api.py
@@ -1,1 +1,15 @@
+import requests
 
+import config
+
+def send_message_to_fb_messenger(recipinet_id: str, message_text: str) -> None:
+    url = f""
+    data = {
+        "recipient": {"id": recipinet_id},
+        "message": {"text": message_text}
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print("Message sent successfully.")
+    else:
+        print("Failed to send message. Status code:", response.status_code)
